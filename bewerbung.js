afafb0508d4cca30936b8db15b596d3fb19e49e1
fambosyt <ytfambos@gmail.com>
2025-04-10 18:41:19 +0200

    Add files via upload

---
+++ bewerbung.js
@@ -0,0 +1,6 @@
+// config.js
+const discordWebhookConfig = {
+    url: 'https://discordapp.com/api/webhooks/1346932430191202436/aVOD8ti4ovZsHXpI90QEFIGcR3zOvoVezKkSS7qitnCrl682P4xGvhmE2OYFVlv9jF3k',
+    username: 'FambosYT Bewerbungen',
+    avatar_url: 'https://fambosyt.de/img/logo.png'
+};
---
+++ kontakt.js
@@ -0,0 +1,6 @@
+// config.js - Discord Webhook Konfiguration
+const discordWebhookConfig = {
+    url: 'https://discordapp.com/api/webhooks/1358036037124886691/diuRnd6MyLcq3Z2O-iLbpahfaN49WKQ8FvFf9Q7LVxyAfHp_K20hbkWxqpEAsc-gRy6O',
+    username: 'FambosYT Kontaktformular',
+    avatar_url: 'https://fambosyt.de/img/logo.png'
+};
