2c85c86275fa0dd8635271bf8d76bf91d747cf21
fambosyt <ytfambos@gmail.com>
2025-04-10 18:44:05 +0200

    Update bewerbung.js

--- bewerbung.js
+++ bewerbung.js
@@ -1,6 +1,6 @@
 // config.js
 const discordWebhookConfig = {
-    url: 'https://discordapp.com/api/webhooks/1346932430191202436/aVOD8ti4ovZsHXpI90QEFIGcR3zOvoVezKkSS7qitnCrl682P4xGvhmE2OYFVlv9jF3k',
-    username: 'FambosYT Bewerbungen',
+    url: 'HIER_DEIN_WEBHOOK_URL',
+    username: 'Bewerbungen',
     avatar_url: 'https://fambosyt.de/img/logo.png'
 };
